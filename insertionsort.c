d762277a60099495142a8aa2b79b52d433b36a76
Thrishank123 <125119917+Thrishank123@users.noreply.github.com>
2023-09-14 23:29:27 +0530

    Insertion Sort

---
+++ insertionsort.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void swap(int a,int b){
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
+    return;
+}
+int main()
+{
+    int n,i,temp,y;
+    printf("Number of elements:\n");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter elements in the Random Order: \n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(y=i;y>=0;y--){
+        if(a[y]>a[y+1])
+        {
+           temp=a[y];
+           a[y]=a[y+1];
+           a[y+1]=temp; 
+        }
+        else
+        break;
+        }
+    }
+    printf("sorted array :\n");
+    for(i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+    return 0;
+
+    
+}
